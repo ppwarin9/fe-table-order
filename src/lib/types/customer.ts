9ef225413bb3fab43b00a7d03624d1b51712855f
ppwarin9 <knotgod3@gmail.com>
2026-07-11 21:04:49 +0700

    chore: add customer type

---
+++ src/lib/types/customer.ts
@@ -0,0 +1,18 @@
+import { ID } from '@/lib/types/common';
+
+export interface Customer {
+  id: ID;
+  lineUserId: string;
+  displayName: string;
+  pictureUrl: string;
+  isOaFriend: boolean;
+  firstSeenAt: string;
+  updatedAt: string;
+}
+
+export interface LineProfile {
+  userId: string;
+  displayName: string;
+  pictureUrl: string;
+  isOaFriend: boolean;
+}
