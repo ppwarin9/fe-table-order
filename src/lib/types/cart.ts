37221bda4113266b81239a38fed0d15d14c5df48
ppwarin9 <knotgod3@gmail.com>
2026-07-11 20:48:46 +0700

    chore: add cart types

---
+++ src/lib/types/cart.ts
@@ -0,0 +1,37 @@
+import { ID, Satang } from '@/lib/types/common';
+
+export interface Cart {
+  id: ID;
+  tableSessionId: ID;
+  updatedAt: string;
+}
+
+export interface CartItem {
+  id: ID;
+  cartId: ID;
+  menuItemId: ID;
+  addedBy: ID;
+  quantity: number;
+  note: string;
+  addedAt: string;
+}
+
+export interface CartItemModifier {
+  cartItemId: ID;
+  modifierOptionId: ID;
+}
+
+export interface CartItemView extends CartItem {
+  menuName: string;
+  unitPrice: Satang;
+  modifiers: { optionId: ID; name: string; priceDelta: Satang }[];
+  addedByName: string;
+  addedByPicture: string;
+}
+
+export interface CartDetail {
+  cart: Cart;
+  items: CartItemView[];
+  totalQuantity: number;
+  totalAmount: Satang;
+}
