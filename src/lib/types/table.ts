759838dd70d20bc8d08a957f1344cf46145133bf
ppwarin9 <knotgod3@gmail.com>
2026-07-12 17:55:39 +0700

    chore: add table type

---
+++ src/lib/types/table.ts
@@ -0,0 +1,28 @@
+import { ID } from '@/lib/types/common';
+
+export interface DiningTable {
+  id: ID;
+  storeId: ID;
+  tableNumber: string;
+  qrToken: string;
+  qrGeneratedAt: string;
+  isActive: boolean;
+}
+
+export type SessionStatus = 'open' | 'closed';
+
+export interface TableSession {
+  id: ID;
+  dinningTableId: ID;
+  status: SessionStatus;
+  openedAt: string;
+  closedAt: string | null;
+}
+
+export interface SessionMember {
+  id: ID;
+  tableSessionId: ID;
+  customerId: ID;
+  sessionToken: string;
+  joinedAt: string;
+}
