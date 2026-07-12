701b5a80e3266b9dc2581484a1580d6a3a19dd33
ppwarin9 <knotgod3@gmail.com>
2026-07-12 17:47:12 +0700

    chore: add staff type

---
+++ src/lib/types/staff.ts
@@ -0,0 +1,18 @@
+import { ID } from '@/lib/types/common';
+
+export type RoleCode = 'owner' | 'manager' | 'staff';
+
+export interface Role {
+  id: ID;
+  code: RoleCode;
+  name: string;
+}
+
+export interface StaffUser {
+  id: ID;
+  storeId: ID;
+  roleId: ID;
+  email: string;
+  name: string;
+  isActive: boolean;
+}
